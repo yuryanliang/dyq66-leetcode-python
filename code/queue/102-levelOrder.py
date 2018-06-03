004405e610f6e3299429b895a1c50f5208fd57e7
dyq <email@example.com>
2018-06-03 11:05:54 +0800

    queue专题-在写知乎文章的时候，对代码进行了优化

--- code/queue/102-levelOrder.py
+++ code/queue/102-levelOrder.py
@@ -6,25 +6,49 @@ __date__ = '2018/6/2 18:01'
 class Solution:
     def levelOrder(self, root):
         """
-        1. 队列存节点, 层数
-        2. 初始化存根节点
-        3. 不断的出队, 将值保存在结果集相应的位置, 将左右节点在加入队列, 注意层数
-        4. 如果当期层数没出现过, 并且对应节点非空则增加一个该层对应的list
+        1. 初始化队列，将头节点保存，由于题目需要根据节点所在的层返回，所以这里队列中的每一项都是一个tuple(节点，层级)，头结点的tuple是（root, 0）
+        2. 不断的出队列，知道队列为空，整个树就被遍历完了。
+        3.  从队列中获取到当前节点，和节点所在的层级，如果节点是空的就继续出队列，否则进行步骤4和5。
+        4.  如果当前的层级是第一次出现，则将结果集中存入一个list，供当前层级存放节点的值。这里使用一个set来判断层级是否是第一次出现，当然还可以使用（len(结果集) - 1 != level ）这种方式，但是不太语义化，本文就没有采取。
+        5. 将节点的值存入结果集中对应位置的list中，将左节点，右节点加入队列
         """
-        # 步骤1, 2
+        # 步骤1
         from collections import deque
         node_queue = deque()
         node_queue.append((root, 0))
 
         res, res_set = [], set()
 
+        # 步骤2
+        while node_queue:
+            # 步骤三
+            node, level = node_queue.popleft()
+            if node:
+                # 步骤四
+                if level not in res_set:
+                    res_set.add(level)
+                    res.append([])
+                # 步骤五
+                res[level].append(node.val)
+                node_queue.append((node.left, level + 1))
+                node_queue.append((node.right, level + 1))
+
+        return res
+
+    def levelOrder02(self, root):
+        """使用res长度的判断来代替set"""
+        from collections import deque
+        node_queue = deque()
+        node_queue.append((root, 0))
+
+        res = []
+
         # 步骤3
         while node_queue:
             node, level = node_queue.popleft()
             # 步骤四
             if node:
-                if level not in res_set:
-                    res_set.add(level)
+                if len(res) - 1 != level:
                     res.append([])
                 res[level].append(node.val)
                 node_queue.append((node.left, level + 1))
